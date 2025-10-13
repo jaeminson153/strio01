6c99541d6db1acf8745138086c30bb087db471ab
sonjaemin <jaeminson153@gmail.com>
2025-10-13 15:24:04 +0900

    init pjt 2025.10.13

---
+++ src/main/java/com/example/strio01/Strio01Application.java
@@ -0,0 +1,13 @@
+package com.example.strio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Strio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Strio01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/strio01/Strio01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strio01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Strio01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
